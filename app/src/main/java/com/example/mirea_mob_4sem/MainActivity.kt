10490470e5d8a6b84de361e6b209ec2c96e2a459
yarosshest <yarosshest@gmail.com>
2023-03-21 04:41:30 +0300

    3 start

--- app/src/main/java/com/example/mirea_mob_4sem/MainActivity.kt
+++ app/src/main/java/com/example/mirea_mob_4sem/MainActivity.kt
@@ -12,24 +12,15 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var editText: EditText
-    private lateinit var v_sdk: TextView
+    private lateinit var textView: TextView
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-            v_sdk = findViewById(R.id.text_sdk)
-            v_sdk.text = "Your version SDK is good"
-        }
-
-        button = findViewById(R.id.button)
-        editText = findViewById(R.id.inputText)
-        button.setOnClickListener{
-            val intent = Intent(this@MainActivity, MainActivity2::class.java)
-            intent.putExtra("text", editText.text.toString())
-            startActivity(intent)
-        }
+        textView = TextView(this)
+        textView.text = "Example"
+        textView.textSize = 30F
+        setContentView(textView)
     }
 }
